27410afb832122984f76dbb1923e96ec55d952fb
sesas <gabreyla@gmail.com>
2012-03-17 20:11:39 -0700

    added the statistical info to the output.
    Signed-off-by: sesas <gabreyla@gmail.com>

--- diceRoller.py
+++ diceRoller.py
@@ -5,10 +5,9 @@ a number is. (normal 6 sided dice would be (1/6, 1/6, 1/6, 1/6, 1/6, 1/6).
 But not for now.
 '''
 import resources as res
-import random
-
+import random 
+import itertools as itr
 r = random
-
 
 class dice:
 
@@ -54,6 +53,19 @@ class statManager:
         self.min = diceObj.num_dice
         self.counter = collections.Counter( range(self.min, self.max+1) )
         self.counter.subtract( range(self.min, self.max+1) )
+        # the following line is slow if using more than 8 dice.
+        # (might want to put on separate thread)
+        self.diceProbab = collections.Counter(map( sum,
+                                           itr.product( range(1,self.dice.sides+1), repeat=self.dice.num_dice)  ) )
+        self.totalPoss = sum( self.diceProbab.values() )
+        self.theor_probab = { num:dp/self.totalPoss for num, dp in self.diceProbab.items() }
+
+    def probDiff(self):
+        nRolls = len(self.rollHist)
+        actualProb = { k:prob/nRolls for k, prob in self.counter.items() }
+        probDiff = { k:(actualProb[k]-self.theor_probab[k]) for k in actualProb }
+        return probDiff
+        pass
 
     def addRoll(self, rollVal):
         self.rollHist.append(rollVal)
@@ -74,7 +86,7 @@ class statManager:
         
 
 class histGraph:
-    defaultLineFormat = "{1: >3}:{4: <3} - {0:{3}>{2}}"
+    defaultLineFormat = "{num: >3}: {count: >3},{diff: >+8.2F} - {0:{pad}>{strLen}}"
     def __init__(self, statObj, linesWidth = 70, lineFormat = defaultLineFormat, padString='#'):
         self.linesWidth = linesWidth
         self.lineFormat = lineFormat
@@ -88,9 +100,11 @@ class histGraph:
         lines  = []
         maxCount = max(counterObj.values())
         charMultiplier = self.linesWidth / (maxCount*len(self.padString))
+        diffDict = self.statObj.probDiff()
         for val, count in sorted(counterObj.items()):
             lines.append( self.lineFormat.format(
-                    '', val, int(count*charMultiplier), self.padString, count) )
+                    '', num=val, strLen=int(count*charMultiplier),
+                    pad=self.padString, count=count, diff=diffDict[val]*100) )
             pass
         return lines
 ##            for item in sorted(counterObj):
@@ -124,6 +138,34 @@ class game:
             print(self.histGraph)
             input()
 
+
+##def poss(nd, ns):
+##    if nd:
+##        gen = poss(nd-1, ns)
+##        
+##        for j in gen:
+##            for r in range(ns):
+##                
+##                yield j + [r+1]
+##    else:
+####        for r in range(ns):
+##            yield []
+##                    
+##print ( collections.Counter( map( sum, [k for k in poss(3, 6)]  ) ) )
+##           
+##print ( collections.Counter( map( sum, [k for k in poss(3, 8)]  ) ) )
+##a = collections.Counter( map( sum, itr.product( range(1,7), repeat=2)  ) )
+##print (a)
+##print (sum(a.values()))
+####for j in range(13):
+##    
+
+import math
+p = 1/6
+##print(p)
+def binom(n, k, p = p):
+    return (n**k / math.factorial(k)) * p**k * (1-p)**(n-k)
+
 if __name__ == "__main__":
 ##    d = dice()
 ##    sm = statManager(d)
@@ -134,5 +176,9 @@ if __name__ == "__main__":
 ##        print( res.num[1] )
 ##        print(h)
 ##        input()    
-##    
+##
+            
     g = game()
+    pass
+
+
